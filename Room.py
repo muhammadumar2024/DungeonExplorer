85b091102ce2c7863589e6ead4c6d01780030d05
Shariq Ashraf <muuk1720577@gmail.com>
2025-03-10 01:32:10 +0500

    Create Room.py

---
+++ Room.py
@@ -0,0 +1,12 @@
+﻿class Room:
+    def __init__(self, description, item=None):
+        self.__description = description
+        self.__item = item
+
+    def get_description(self):
+        """Returns the room's description."""
+        return self.__description
+
+    def get_item(self):
+        """Returns the item in the room (if any)."""
+        return self.__item
